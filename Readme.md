451548e89fe0ace45890f1e9033e01715abc1ffa
Valk <wayne@yrpnetworks.com.au>
2018-11-16 13:52:22 +1100

    Create base project

---
+++ Readme.md
@@ -0,0 +1,7 @@
+This is a sample template for a peachpie site running Wordpress
+
+A few things of note:
+
+- If your site needs to scan for php files then you need to copy the php files to the output folder. Use the Library template for making Patches, Plugins and Theme modules.
+- I recommend adding modules (plugins, themes, patches) as sub-modules to make management easier.
+- The peachpie version is configured in Common.props but for libraries you have to set the SDK version (look at the <Project Sdk=> near the top of the .msbuildproj file)
---
+++ build.ps1
@@ -0,0 +1,43 @@
+param (
+    [string]$build
+)
+
+if($build -ne "Release" -and $build -ne "Debug")
+{
+    Write-Host("Need to set build parameter to either 'Release' or 'Debug'");
+    return;
+}
+
+
+$basePath = Get-Location;
+
+Get-ChildItem "$basePath/src/Patches" -Directory |
+ForEach-Object {
+    Set-Location $_.FullName;
+    Invoke-Expression "dotnet build -c $build --force";
+}
+
+Get-ChildItem "$basePath/src/Plugins" -Directory |
+ForEach-Object {
+    Set-Location $_.FullName;
+    Invoke-Expression "dotnet build -c $build --force";
+}
+
+Get-ChildItem "$basePath/src/Themes" -Directory |
+ForEach-Object {
+    Set-Location $_.FullName;
+    Invoke-Expression "dotnet build -c $build --force";
+}
+
+if($build -eq "Debug")
+{
+    Set-Location "$basePath/src/Server";
+    Invoke-Expression "dotnet build -c $build --force";
+}
+if($build -eq "Release")
+{
+    Set-Location "$basePath/src/Server";
+    Invoke-Expression "dotnet publish -c $build --force";
+}
+
+Set-Location $basePath;
