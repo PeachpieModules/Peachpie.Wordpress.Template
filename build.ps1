306049e0d4bd307dd5b61d3b6a79df341309ec41
Valks <Valks@users.noreply.github.com>
2018-11-18 01:16:18 +1030

    Azure pipelines (#2)
    * Try to rework without powershell script so it will build the deployment artifacts.
    
    * I think the @2 refers to the revision of the task
    
    * Added build status badge.
    
    * Updated pipeline to build server outside powershell script.
    Helps with artifact deployment.
    
    * [NIT] Changed logging notes to make the steps clearer
    
    * Follow the offical example.
    
    * Added wwwroot folder for azure-pipelines to detect it's a web project.
    
    * Disable publish web projects setting.
    Hopefully not doing anything important.
    
    * Add step to run the publish task on the staging directory output.
    
    * [NIT] Display name cleanup.
    
    * Updated AspNet.Core version to 2.1.6
    
    * Added Deployment status to readme.md

--- build.ps1
+++ build.ps1
@@ -8,43 +8,36 @@ if($build -ne "Release" -and $build -ne "Debug")
     return;
 }
 
-Write-Host("Building Wordpress: $build");
+Write-Host("- Building Wordpress Modules: $build");
 
 $basePath = Get-Location;
 
+Write-Host("Building Patches.");
 Get-ChildItem "$basePath/src/Patches" -Directory |
 ForEach-Object {
     Set-Location $_.FullName;
     Invoke-Expression "dotnet build -c $build --force";
 }
 
-Get-ChildItem "$basePath/src/Plugins" -Directory |
-ForEach-Object {
-    Set-Location $_.FullName;
-    Invoke-Expression "dotnet build -c $build --force";
-}
-
+Write-Host("Building Must Use Plugins.");
 Get-ChildItem "$basePath/src/MustUsePlugins" -Directory |
 ForEach-Object {
     Set-Location $_.FullName;
     Invoke-Expression "dotnet build -c $build --force";
 }
 
-Get-ChildItem "$basePath/src/Themes" -Directory |
+Write-Host("Building Plugins.");
+Get-ChildItem "$basePath/src/Plugins" -Directory |
 ForEach-Object {
     Set-Location $_.FullName;
     Invoke-Expression "dotnet build -c $build --force";
 }
 
-if($build -eq "Debug")
-{
-    Set-Location "$basePath/src/Server";
+Write-Host("Building Themes.");
+Get-ChildItem "$basePath/src/Themes" -Directory |
+ForEach-Object {
+    Set-Location $_.FullName;
     Invoke-Expression "dotnet build -c $build --force";
-}
-if($build -eq "Release")
-{
-    Set-Location "$basePath/src/Server";
-    Invoke-Expression "dotnet publish -c $build --force";
 }
 
 Set-Location $basePath;
