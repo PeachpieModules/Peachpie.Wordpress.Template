7c478bc61774821c2e49f1d3c1da96b05de870b8
Wayne Adams <wayne@yrpnetworks.com.au>
2020-02-17 19:13:18 +1100

    Update the template

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using PeachPied.WordPress.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace Server
 {
@@ -11,10 +11,11 @@ namespace Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -22,6 +23,7 @@ namespace Server
             }
             if(env.IsStaging() || env.IsProduction())
             {
+                app.UseResponseCompression();
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
